9f1c7b1dcc6883e00a10261787468ad797615d18
Dovydas Pacerzinskas <sadywoodhq@gmail.com>
2025-12-04 00:03:40 +0100

    AuthService.java now verifies token and extracts users info

---
+++ src/main/java/com/ruby/pro203_exam/google/service/AuthService.java
@@ -0,0 +1,62 @@
+package com.ruby.pro203_exam.google.service;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+@Service
+@Slf4j
+public class AuthService {
+
+    @Value("${google.client-ids}")
+    private List<String> clientIdList;
+
+    public GoogleIdToken.Payload verifyToken(String idTokenString)
+        throws GeneralSecurityException, IOException {
+        log.debug("Verifying token");
+
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(
+                new NetHttpTransport(),
+                GsonFactory.getDefaultInstance())
+                        .setAudience(clientIdList)
+                        .build();
+        GoogleIdToken idToken = verifier.verify(idTokenString);
+
+        if (idToken == null) {
+            GoogleIdToken.Payload payload = idToken.getPayload();
+            log.debug("Verified token for user: {}", payload.getEmail());
+            return payload;
+        }else {
+            log.error("Invalid token");
+            throw new SecurityException("Invalid token");
+        }
+    }
+
+    public UserInfoFromGoogle extractUserInfo(GoogleIdToken.Payload payload){
+        return UserInfoFromGoogle.builder()
+                .email(payload.getEmail())
+                .googleId(payload.getSubject())
+                .name((String) payload.get("name"))
+                .profilePictureUrl((String) payload.get("picture"))
+                .emailVerified(payload.getEmailVerified())
+                .build();
+    }
+
+    @lombok.Data
+    @lombok.Builder
+    public static class UserInfoFromGoogle {
+        private String email;
+        private String googleId;
+        private String name;
+        private String profilePictureUrl;
+        private boolean emailVerified;
+    }
+}
