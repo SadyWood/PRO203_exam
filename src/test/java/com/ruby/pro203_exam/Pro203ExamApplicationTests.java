7813f7a09a551153e1979490f69c449722ce0bf3
Dovydas Pacerzinskas <sadywoodhq@gmail.com>
2025-12-03 19:34:14 +0100

    Initializer

---
+++ src/test/java/com/ruby/pro203_exam/Pro203ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruby.pro203_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro203ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
