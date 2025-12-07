ab2f2e4a25574a8ae6cfbd912cd5e7df79ff203b
Dovydas Pacerzinskas <sadywoodhq@gmail.com>
2025-12-07 21:06:16 +0100

    Staff service to manage functions such as getAllStaff, getStaffById, and of course in what form factor

---
+++ src/main/java/com/ruby/pro203_exam/staff/service/StaffService.java
@@ -0,0 +1,51 @@
+package com.ruby.pro203_exam.staff.service;
+
+import com.ruby.pro203_exam.staff.dto.ResponseDto;
+import com.ruby.pro203_exam.staff.model.Staff;
+import com.ruby.pro203_exam.staff.repository.StaffRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional(transactionManager = "appTransactionManager")
+public class StaffService {
+    private final StaffService staffService;
+    private final StaffRepository staffRepository;
+
+    public List<ResponseDto> getAllStaff() {
+        log.info("Get all staff");
+        return StreamSupport.stream(staffRepository.findAll().spliterator(), false)
+                .map(this::toResponseDto)
+                .collect(Collectors.toList());
+    }
+
+    public ResponseDto getStaffById(UUID id){
+        log.info("Get staff by id: {}", id);
+        Staff staff = staffRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No staff found with id: " + id));
+        return toResponseDto(staff);
+    }
+
+    private ResponseDto toResponseDto(Staff staff) {
+        return ResponseDto.builder()
+                .id(staff.getId())
+                .firstName(staff.getFirstName())
+                .lastName(staff.getLastName())
+                .email(staff.getEmail())
+                .employeeId(staff.getEmployeeId())
+                .phoneNr(staff.getPhoneNumber())
+                .position(staff.getPosition())
+                .build();
+
+    }
+
+}
