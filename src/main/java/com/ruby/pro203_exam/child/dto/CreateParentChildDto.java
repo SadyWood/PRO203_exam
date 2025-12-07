775ccd8067519b91cb771fb7f7b1611edf7ff448
Dovydas Pacerzinskas <sadywoodhq@gmail.com>
2025-12-07 21:23:20 +0100

    Dto for creating a new relationship status between a parent and child.

---
+++ src/main/java/com/ruby/pro203_exam/child/dto/CreateParentChildDto.java
@@ -0,0 +1,23 @@
+package com.ruby.pro203_exam.child.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateParentChildDto {
+    private UUID parentId;
+    private UUID childId;
+    private String RelationStatus;
+    private Boolean canCheckOut;
+    private Boolean canCheckIn;
+    private Boolean isPrimaryContact;
+    private Boolean requiredVerification;
+
+}
