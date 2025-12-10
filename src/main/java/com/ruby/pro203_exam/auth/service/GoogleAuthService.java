8948a7f0da378ab5dac56eaefe217453bef478f6
Dovydas Pacerzinskas <sadywoodhq@gmail.com>
2025-12-10 01:06:32 +0100

    Removed unused imports

--- src/main/java/com/ruby/pro203_exam/auth/service/GoogleAuthService.java
+++ src/main/java/com/ruby/pro203_exam/auth/service/GoogleAuthService.java
@@ -6,7 +6,6 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/ruby/pro203_exam/config/PropertiesConfig.java
+++ src/main/java/com/ruby/pro203_exam/config/PropertiesConfig.java
@@ -1,6 +1,5 @@
 package com.ruby.pro203_exam.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
