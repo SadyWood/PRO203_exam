58cc87a0cb7a1066905c21fec604c9cbcb4da707
Dovydas Pacerzinskas <sadywoodhq@gmail.com>
2025-12-11 16:14:00 +0100

    Test set to always pass. Ready for further implementation of tests in future.

--- src/test/java/com/ruby/pro203_exam/Pro203ExamApplicationTests.java
+++ src/test/java/com/ruby/pro203_exam/Pro203ExamApplicationTests.java
@@ -3,11 +3,14 @@ package com.ruby.pro203_exam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class Pro203ExamApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
